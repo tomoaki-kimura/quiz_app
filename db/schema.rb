d5e7f4036054e12fa8d7b54533acbb0755836c45
Your Name <you@example.com>
2019-04-14 20:35:27 +0000

    2_03_console

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_14_190735) do
+ActiveRecord::Schema.define(version: 2019_04_14_200906) do
 
   create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 2019_04_14_190735) do
     t.string "a_3"
     t.string "a_4"
     t.integer "answer"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
