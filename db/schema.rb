c9e2a62c0b6d054ac6a73f220fc90c53ea537b3b
tomoaki-kimura <tkim19750404@gmail.com>
2021-07-14 07:50:15 +0900

    add user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_13_222619) do
+ActiveRecord::Schema.define(version: 2021_07_13_224226) do
 
   create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "content"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 2021_07_13_222619) do
     t.string "a_3"
     t.string "a_4"
     t.integer "answer"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
