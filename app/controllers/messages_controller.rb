35d1e71a405058db5294ca4f0371fdc89eb67115
tomoaki-kimura <tkim19750404@gmail.com>
2021-07-15 14:07:50 +0900

    modify messages controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,26 @@
 class MessagesController < ApplicationController
+
   def index
     @messages = Message.all.order(created_at: :asc)
     @message = Message.new
+  end
+
+  def create
+    @message = Message.new(message_params)
+
+    if @message.save
+      flash[:success] = 'Message が正常に投稿されました'
+      redirect_to root_url
+    else
+      @messages = Message.all.order(created_at: :asc)
+      flash.now[:danger] = 'Message が投稿されませんでした'
+      render :index
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content)
   end
 end
