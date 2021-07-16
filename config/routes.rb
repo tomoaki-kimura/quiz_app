208e13f5b6166b0563b21cddf663b6c4219a00e8
tomoaki-kimura <tkim19750404@gmail.com>
2021-07-16 09:13:45 +0900

    add admin session

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :questions
   root 'messages#index'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  resources :questions
   resources :messages
 end
